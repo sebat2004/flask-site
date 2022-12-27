f768df94976e415f2f7df52660f049da42f31483
Seba <sebastiantorresola3@gmail.com>
2022-12-27 01:38:59 -0400

    added register.html and forms.py
    modified base.html to support error-reading and used bootstrap to help format the register.html page

--- flask-site.py
+++ flask-site.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "sebayeva"
+    app.config['SECRET_KEY'] = "3e63407c98351c6073f49fc8976ee533"
 
     return app
 
 app = create_app()
+
 
 people = [
     {
@@ -18,16 +20,35 @@ people = [
     'name': "Eva",
     'age' : 16,
     'job' : "Sushi Bar"
-    }
+    },
+    {
+    'name': "Jose Luis Torresola",
+    'age': "???",
+    'job': "retired"
+    }    
 ]
+
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template('home.html', people=people)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():   
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', form=form)
+
+@app.route("/login")
+def login():   
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
